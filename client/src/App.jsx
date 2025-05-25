368af11cbe78808999754eaa98508921c5b97343
IshantRaut <ishjee2018@gmail.com>
2025-05-25 17:11:05 +0530

    Created NewsLetter and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { Routes, useLocation,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import AllRooms from './components/AllRooms';
 function App() {
 
   const isOwenerPath = useLocation().pathname.includes('/owner');
@@ -15,6 +16,7 @@ function App() {
     <div className='min-h-[70vh'>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<AllRooms />} />
       </Routes>
 
     </div>
