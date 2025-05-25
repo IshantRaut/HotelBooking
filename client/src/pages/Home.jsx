7a745e298a6229f81910647b70d3969b2b91d6c0
IshantRaut <ishjee2018@gmail.com>
2025-05-25 14:59:34 +0530

    Exclusive Offers

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import HotelCard from '../components/HotelCard'
 import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
     </>
   )
 }
